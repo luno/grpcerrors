c0c560b635987af40bbfa8fd066780579a27399d
Neil Garb <neil.garb@gmail.com>
2019-02-01 14:49:34 +0200

    Add comments

--- grpcerrors.go
+++ grpcerrors.go
@@ -1,3 +1,16 @@
+// Package grpcerrors helps transmit errors over gRPC by associating errors
+// with status codes which can be transmitted safely.
+//
+// In the server:
+//   return nil, grperrors.ToGrpc(err)
+//
+// In the client:
+//   _, err := grpcCall()
+//   if grpcerrors.FromGrpc(err) == someError { ... }
+//
+// In both client and server:
+//   someError := errors.New("some error")
+//   grpcerrors.Register(1, someError)
 package grpcerrors
 
 import "errors"
@@ -11,14 +24,17 @@ func (e *grpcError) Error() string {
 	return e.err.Error()
 }
 
+// Registry holds mappings from error codes to errors.
 type Registry struct {
 	errors map[int]error
 }
 
+// NewRegistry returns a new error code registry.
 func NewRegistry() *Registry {
 	return &Registry{errors: make(map[int]error)}
 }
 
+// Register associates a status code with an error.
 func (r *Registry) Register(code int, err error) error {
 	_, ok := r.errors[code]
 	if ok {
@@ -28,6 +44,9 @@ func (r *Registry) Register(code int, err error) error {
 	return nil
 }
 
+// ToGrpc converts the given error to a gRPC error by looking the error's code
+// up in the registry. If the error hasn't been registered, the error is
+// returned unchanged.
 func (r *Registry) ToGrpc(err error) error {
 	for k, v := range r.errors {
 		if v == err {
@@ -36,7 +55,10 @@ func (r *Registry) ToGrpc(err error) error {
 	}
 	return err
 }
-
+ 
+// FromGrpc converts a gRPC error back to the registered error type. If the
+// error isn't a gRPC error, or the status code isn't registered, the error
+// is returned as is.
 func (r *Registry) FromGrpc(err error) error {
 	if err == nil {
 		return nil
@@ -54,14 +76,18 @@ func (r *Registry) FromGrpc(err error) error {
 
 var defaultRegistry Registry
 
+// Register registers an error code with the default registry.
 func Register(code int, err error) {
 	defaultRegistry.Register(code, err)
 }
 
+// ToGrpc converts the given error to a gRPC error using the default registry.
 func ToGrpc(err error) error {
 	return defaultRegistry.ToGrpc(err)
 }
 
+// FromGrpc converts a gRPC back to a registered error using the default
+// registry.
 func FromGrpc(err error) error {
 	return defaultRegistry.FromGrpc(err)
 }
